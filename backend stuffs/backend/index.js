9217f47a6c0bba6fe9a46f9215b44f048574d110
Hope Spencer <61034586+JamieSpencerBSWD@users.noreply.github.com>
2026-04-22 21:16:34 -0500

    setting up stuff for backend

---
+++ backend stuffs/backend/index.js
@@ -0,0 +1,3 @@
+//This is where I will make and relearn Node and JavaScript as well as Express for creating API's :3
+
+//Backend will be created with Express/NodeJS and the databse will be managed through MYSQL
