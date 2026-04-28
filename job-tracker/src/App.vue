e2d8ef1b1a755cb786e10bade54e2222dea8dc0f
Hope Spencer <61034586+JamieSpencerBSWD@users.noreply.github.com>
2026-04-28 17:00:59 -0500

    implementing vue, setting up, and getting fetch working

--- job-tracker/src/App.vue
+++ job-tracker/src/App.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import JobList from './components/JobList.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <JobList/>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- job-tracker/src/components/JobList.vue
+++ job-tracker/src/components/JobList.vue
@@ -1,33 +1,69 @@
 <script setup lang="ts">
+import dayjs from 'dayjs';
 import { ref } from 'vue';
     interface Job {
         id: string;
         position: string;
         company: string;
-        status: string;
+        application_status: string;
         date_applied: string;
-        location: string;
+        updated_at: string;
+        job_location: string;
         company_location: string;
         post_link: string;
         notes: string;
     }
     const jobs = ref<Job[]>([])
-
-    fetch("http://localhost:3000/jobs/").then(response=>response.json()).then(jsonData=>{
-        jobs.value = jsonData;
-    }).catch(err=> {
-      console.log(err)
-    })
+    const getJobs = async() => {
+        try {
+            await fetch("http://localhost:3000/jobs/").then(response=>response.json()).then(jsonData=>{
+            jobs.value = jsonData;
+        }).catch(err=> {
+        console.log(err)
+        })
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }
+    getJobs()
 </script>
 <template>
+    <h1>Job Tracker</h1>
     <div>
-        <ul>
-            <li v-for="job in jobs" :key="job.id">
-                <p>Position: {{ job.position }}</p>
-                <p>Company: {{ job.company }}</p>
-            </li>
-        </ul>
+        <thead>
+            <tr>
+                <th scope="col">Position</th>
+                <th scope="col">Company</th>
+                <th scope="col">Application Status</th>
+                <th scope="col">Date Applied</th>
+                <th scope="col">Date Updated</th>
+                <th scope="col">Location</th>
+                <th scope="col">Company Location</th>
+                <th scope="col">Post Link</th>
+                <th scope="col">Notes</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="job in jobs" :key="job.id">
+                <td>{{ job.position }}</td>
+                <td>{{ job.company }}</td>
+                <td>{{ job.application_status }}</td>
+                <td>{{ dayjs(job.date_applied ).format('MM-DD-YYYY hh:mmA') }}</td>
+                <td>{{ dayjs(job.updated_at ).format('MM-DD-YYYY hh:mmA')  }}</td>
+                <td>{{ job.job_location }}</td>
+                <td>{{ job.company_location }}</td>
+                <td>{{ job.post_link }}</td>
+                <td>{{ job.notes }}</td>
+            </tr>
+        </tbody>
     </div>
 </template>
 <style scoped>
+th{
+    padding: 5px;
+}
+td{
+    padding: 5px;
+}
 </style>
