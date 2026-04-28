9c002e26a18991242e0ec51aceaaad8b589e8ead
Hope Spencer <61034586+JamieSpencerBSWD@users.noreply.github.com>
2026-04-28 15:54:06 -0500

    getting frontend working

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,14 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.get('/', routes.index)
 app.get('/jobs', routes.jobs)
---
+++ job-tracker/src/components/JobList.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+    interface Job {
+        id: string;
+        position: string;
+        company: string;
+        status: string;
+        date_applied: string;
+        location: string;
+        company_location: string;
+        post_link: string;
+        notes: string;
+    }
+    const jobs = ref<Job[]>([])
+
+    fetch("http://localhost:3000/jobs/").then(response=>response.json()).then(jsonData=>{
+        jobs.value = jsonData;
+    }).catch(err=> {
+      console.log(err)
+    })
+</script>
+<template>
+    <div>
+        <ul>
+            <li v-for="job in jobs" :key="job.id">
+                <p>Position: {{ job.position }}</p>
+                <p>Company: {{ job.company }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<style scoped>
+</style>
